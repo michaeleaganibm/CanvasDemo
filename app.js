32c904ea45f5b0e0d243de814c55ba67859e425b
Michael Eagan <sfdc_dev@eagano.com>
2024-03-25 18:15:22 -0500

    fixup tree call

--- app.js
+++ app.js
@@ -20,18 +20,12 @@ app.post('/', function (req, res) {
   var bodyArray = req.body.signed_request.split(".");
     var consumerSecret = bodyArray[0];
     var encoded_envelope = bodyArray[1];
-    console.log('CS: ' + consumerSecret);
-    console.log('envelope: ' + encoded_envelope);
 
     var check = crypto.createHmac("sha256", consumerSecretApp).update(encoded_envelope).digest("base64");
     console.log('check: ' + check);
 
     if (check === consumerSecret) { 
         var envelope = JSON.parse(Buffer.from(encoded_envelope, "base64").toString("ascii"));
-        //req.session.salesforce = envelope;
-        //console.log("got the session object:");
-        //console.log(envelope);
-        console.log('JSON: ' + JSON.stringify(envelope));
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope) });
     }else{
         res.send("authentication failed");
