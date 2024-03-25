be2c315c48bca8fe8744b2e943389fe68c23b3c2
Michael Eagan <sfdc_dev@eagano.com>
2024-03-25 16:52:09 -0500

    body-parser update (extended)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ console.log('consumer secret - '+consumerSecretApp);
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
  
