564b39f54b46e81f974396e4d5b15e6798e52848
Michael Eagan <sfdc_dev@eagano.com>
2024-03-25 17:39:27 -0500

    debugging

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ bodyParser = require('body-parser'),
 path = require('path') 
 var app = express();
 var crypto = require("crypto");
+const { encode } = require('punycode');
 var consumerSecretApp = process.env.CANVAS_CONSUMER_SECRET;
 
 app.use(express.static(path.join(__dirname, 'views')));
@@ -19,8 +20,11 @@ app.post('/', function (req, res) {
   var bodyArray = req.body.signed_request.split(".");
     var consumerSecret = bodyArray[0];
     var encoded_envelope = bodyArray[1];
+    console.log('CS: ' + consumerSecret);
+    console.log('envelope: ' + encoded_envelope);
 
     var check = crypto.createHmac("sha256", consumerSecretApp).update(encoded_envelope).digest("base64");
+    console.log('check: ' + check);
 
     if (check === consumerSecret) { 
         var envelope = JSON.parse(new Buffer(encoded_envelope, "base64").toString("ascii"));
