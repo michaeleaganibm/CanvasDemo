e7e9e35c37d6a9cbaa6f2ab9ddf4c34f546f39cc
Michael Eagan <sfdc_dev@eagano.com>
2024-08-13 10:17:20 -0500

    Frame to vf/c page

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.post('/', function (req, res) {
     var check = crypto.createHmac("sha256", consumerSecretApp).update(encoded_envelope).digest("base64");
     if (check === consumerSecret) { 
         var envelope = JSON.parse(Buffer.from(encoded_envelope, "base64").toString("ascii"));
-        //res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://eagan-ibm-dev-ed.my.salesforce.com https://eagan-ibm-dev-ed.lightning.force.com');
+        res.header('X-Frame-Options', 'SAMEORIGIN');
+        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://eagan-ibm-dev-ed--c.vf.force.com');
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope) });
     } else {
         res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com');
+        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://eagan-ibm-dev-ed--c.vf.force.com');
         res.send("authentication failed");
     } 
 })
