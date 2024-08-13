72993c48aca2303ca5ae45862e71829ec51856ea
Michael Eagan <sfdc_dev@eagano.com>
2024-08-13 10:10:35 -0500

    qualified

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/', function (req, res) {
     if (check === consumerSecret) { 
         var envelope = JSON.parse(Buffer.from(encoded_envelope, "base64").toString("ascii"));
         //res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.salesforce.com https://*.force.com');
+        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://eagan-ibm-dev-ed.my.salesforce.com https://eagan-ibm-dev-ed.lightning.force.com');
         //res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com');
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope) });
     } else {
