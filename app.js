5d4fc42497e7347bc36b12fe0247d7042cf349f3
Michael Eagan <sfdc_dev@eagano.com>
2024-08-13 09:58:41 -0500

    Remove trailing slash

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ app.post('/', function (req, res) {
     if (check === consumerSecret) { 
         var envelope = JSON.parse(Buffer.from(encoded_envelope, "base64").toString("ascii"));
         res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com/');
+        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com');
         res.render('index', { title: envelope.context.user.userName, req : JSON.stringify(envelope) });
     } else {
         res.header('X-Frame-Options', 'SAMEORIGIN');
-        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com/');
+        res.header('Content-Security-Policy', 'frame-ancestors \'self\' https://*.lightning.force.com');
         res.send("authentication failed");
     } 
 })
